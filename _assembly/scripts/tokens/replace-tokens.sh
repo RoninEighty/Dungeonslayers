879832efddaac156e8f8af097a44046b3f8b3c0e
Friedemann Schneider <friedemann@f-space.de>
2025-03-05 14:23:03 +0100

    smaller fixes.

--- _assembly/scripts/tokens/replace-tokens.sh
+++ _assembly/scripts/tokens/replace-tokens.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-berserker.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-blutmagier.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-daemonologe.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-druide.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-elementarist.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-erzmagier.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-kampfmoench.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-kleriker.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-kriegszauberer.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-meisterdieb.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-nekromant.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-paladin.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-waffenmeister.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-waldlaeufer.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-berserker.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-blutmagier.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-daemonologe.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-druide.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-elementarist.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-erzmagier.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-kampfmoench.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-kleriker.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-kriegszauberer.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-meisterdieb.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-nekromant.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-paladin.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-waffenmeister.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-heldenklassen-waldlaeufer.md
 
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-heiler.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-krieger.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-schwarzmagier.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-spaeher.md
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-zauberer.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-heiler.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-krieger.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-schwarzmagier.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-spaeher.md
+#./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/charaktere-klasse-zauberer.md
 
-./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/talente.md
 ./_assembly/scripts/tokens/replace-tokens.pl _assembly/scripts/tokens/tokens-grw-links.txt ./grw/zaubersprueche.md
