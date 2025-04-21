ab133b004d3c1e267e480cd28f6b45f322d962f9
Friedemann Schneider <friedemann@f-space.de>
2025-04-21 10:30:39 +0200

    URL

--- _assembly/scripts/make-html.sh
+++ _assembly/scripts/make-html.sh
@@ -29,10 +29,10 @@ rm -f alle-seiten.md.tmp
 
 #BASE_URL=https://ronineighty.github.io/Dungeonslayers
 #BASE_URL_ESCAPED=https:\\\/\\\/ronineighty.github.io\\\/Dungeonslayers
-#BASE_URL=https://fschne.github.io/Dungeonslayers
-#BASE_URL_ESCAPED=https:\\\/\\\/fschne.github.io\\\/Dungeonslayers
-BASE_URL=http://localhost/f-space/ds4srd
-BASE_URL_ESCAPED=http:\\\/\\\/localhost\\\/f-space\\\/ds4srd
+BASE_URL=https://fschne.github.io/Dungeonslayers
+BASE_URL_ESCAPED=https:\\\/\\\/fschne.github.io\\\/Dungeonslayers
+#BASE_URL=http://localhost/f-space/ds4srd
+#BASE_URL_ESCAPED=http:\\\/\\\/localhost\\\/f-space\\\/ds4srd
 
 # Inject BASE_URL_ESCAPED in style.css
 perl -pi -e "s/BASE_URL/${BASE_URL_ESCAPED}/g" styles/fonts.css
